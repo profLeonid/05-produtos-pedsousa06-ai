fbdb0f96b43e70aa896b8d9356a3b98782572fd4
PEDRO HENRIQUE DE SOUSA GOMES <25203650@dsn127m1249389.cfp127aluno.local>
2026-02-24 10:41:07 -0300

    Cadastro de codigo, quantidade e produto

--- main.js
+++ main.js
@@ -5,20 +5,37 @@ const botao = document.getElementById('botao')
 botao.addEventListener('click', function () {
 
     const lista = document.getElementById('lista')
+    const codigo = document.getElementById('codigo')
     const produto = document.getElementById('produto')
+    const quantidade = document.getElementById('quantidade')
 
-    if (produto.value.trim() === '') {
-        alert('Digite um produto válido!')
+    // 🔎 Validação
+    if (
+        codigo.value.trim() === '' ||
+        produto.value.trim() === '' ||
+        quantidade.value.trim() === ''
+    ) {
+        alert('Preencha todos os campos!')
         return
     }
 
     // 🗑 Remove produto anterior
     lista.innerHTML = ''
 
-    const span = document.createElement('p')
-    span.textContent = produto.value
+    // 📦 Criando elemento
+    const item = document.createElement('div')
+    item.classList.add('border', 'p-3', 'rounded', 'bg-gray-100')
 
-    lista.appendChild(span)
+    item.innerHTML = `
+        <p><strong>Código:</strong> ${codigo.value}</p>
+        <p><strong>Produto:</strong> ${produto.value}</p>
+        <p><strong>Quantidade:</strong> ${quantidade.value}</p>
+    `
 
+    lista.appendChild(item)
+
+    // 🔄 Limpar campos
+    codigo.value = ''
     produto.value = ''
+    quantidade.value = ''
 })
